578f8f1220c71dd36c2384be6f8810e290201498
Graham Coster <graham.coster@gmail.com>
2019-01-08 13:38:59 +1100

    Exercise completed

--- 6_Building_Interactive_JavaScript_Websites/2_Piano_keys/main.js
+++ 6_Building_Interactive_JavaScript_Websites/2_Piano_keys/main.js
@@ -6,13 +6,22 @@ keys.forEach(function(key){
 })
 
 // Write named functions that change the color of the keys below
+let keyPlay = (event) => {
+  event.target.style.backgroundColor = "grey";
+}
 
+let keyReturn = (event) => {
+  event.target.style.backgroundColor = '';
+}
 
 // Write a named function with event handler properties
-
+function addKeyEventListeners(note) {
+    note.onmousedown = keyPlay;
+    note.onmouseup = keyReturn;
+}
 
 // Write a loop that runs the array elements through the function
-
+notes.forEach( note => { addKeyEventListeners(note) } );
 
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
@@ -29,12 +38,44 @@ nextThree.hidden = true;
 startOver.hidden= true;
 
 // Write anonymous event handler property and function for the first progress button
-
+nextOne.onclick = function(event) {
+  nextOne.hidden = true;
+  nextTwo.hidden = false;
+  document.getElementById("letter-note-five").innerHTML = "D";
+  document.getElementById("letter-note-six").innerHTML = "C";
+}
 
 // Write anonymous event handler property and function for the second progress button
+nextTwo.onclick = function(event) {
+  nextTwo.hidden = true;
+  nextThree.hidden = false;
+  document.getElementById("word-five").innerHTML = "DEAR";
+  document.getElementById("word-six").innerHTML = "FRI-";
+  lastLyric.style.display = 'inline-block';
+  document.getElementById("letter-note-three").innerHTML = "G";
+  document.getElementById("letter-note-four").innerHTML = "E";
+  document.getElementById("letter-note-five").innerHTML = "C";
+  document.getElementById("letter-note-six").innerHTML = "B";
+}
 
 
 // Write anonymous event handler property and function for the third progress button
+nextThree.onclick = function(event) {
+  nextThree.hidden = true;
+  startOver.hidden = false;
+  document.getElementById("word-five").innerHTML = "TO";
+  document.getElementById("word-six").innerHTML = "YOU!";
+
+  document.getElementById("letter-note-one").innerHTML = "F";
+  document.getElementById("letter-note-two").innerHTML = "F";
+  document.getElementById("letter-note-three").innerHTML = "E";
+  document.getElementById("letter-note-four").innerHTML = "C";
+  document.getElementById("letter-note-five").innerHTML = "D";
+  document.getElementById("letter-note-six").innerHTML = "C";
+
+  lastLyric.style.display = 'none';
+
+}
 
 
 // This is the event handler property and function for the startOver button
